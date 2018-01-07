3b9101d17fa01730881e4c433c3a0c403fdd5fe1
Kophi <philipp.kopf@gmail.com>
2018-01-07 21:52:02 +0100

    Standby: lcd line break fixed; lcd timeout added

--- miniQ/standby.ino
+++ miniQ/standby.ino
@@ -1,19 +1,34 @@
+#define DISPLAY_TIMEOUT 6000
+
+unsigned long standbyMillis = 0;
+
 void mStandby()
 {
   if (modeChanged)
   {
-     modeChanged = false;
-    
+    modeChanged = false;
+
     lcd.print("Standby");
+    lcd.setCursor(0, 1);
     lcd.print("Press KEY1");
-    //lcd.noBacklight();
+
     beep(2000);
     delay(10);
     beep(1500);
     delay(10);
     beep(1000);
+
+    standbyMillis = millis();
   }
   else if (currentKey == 1) {
+    lcd.backlight();
     changeMode(M_MENU);
-  } 
+  }
+  else
+  {
+    if (millis() - standbyMillis > DISPLAY_TIMEOUT)
+    {
+      lcd.noBacklight();
+    }
+  }
 }
